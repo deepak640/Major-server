ae2febc1ae12193e7723e27b5156e8c0bc1f8d99
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-03-31 20:17:04 +0530

    fixing database

--- DB/connectivity.js
+++ DB/connectivity.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+const dotenv = require("dotenv")
+dotenv.config({ path: "./config.env" })
+mongoose.connect(process.env.D_B, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB');
     })
